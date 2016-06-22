a50f0fa6bcbadabfb8c06671ee3e4b6d69dfba4d
Gaurav Roy <gpuli@users.noreply.github.com>
2016-06-22 00:48:34 -0400

    Create TreePreorder.cpp
    Input Format
    
    Our hidden tester code passes the root node of a binary tree to your preOrder function.
    
    Output Format
    
    Print the tree's preorder traversal as a single line of space-separated values.

---
+++ TreePreorder.cpp
@@ -0,0 +1,19 @@
+/* you only have to complete the function given below.  
+Node is defined as  
+
+struct node
+{
+    int data;
+    node* left;
+    node* right;
+};
+
+*/
+
+void Preorder(node *root) {
+    cout << root->data << " ";
+    if(root->left!=NULL)
+        Preorder(root->left);
+    if(root->right!=NULL)
+        Preorder(root->right);
+}
