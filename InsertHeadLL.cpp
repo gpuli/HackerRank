bea7debfb1da57b6460452715b3410556d3a571d
Gaurav Roy <gpuli@users.noreply.github.com>
2016-06-22 00:32:38 -0400

    Create InsertHeadLL.cpp
    Input Format 
    You have to complete the Node* Insert(Node* head, int data) method which takes two arguments - the head of the linked list and the integer to insert. You should NOT read any input from stdin/console.
    
    Output Format 
    Insert the new node at the head and return the head of the updated linked list. Do NOT print anything to stdout/console.

---
+++ InsertHeadLL.cpp
@@ -0,0 +1,19 @@
+/*
+  Insert Node at the begining of a linked list
+  Initially head pointer argument could be NULL for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+return back the pointer to the head of the linked list in the below method.
+*/
+Node* Insert(Node *head,int data)
+{
+  Node *nu = new Node();
+  nu->data = data;
+  nu->next = head;
+  head = nu;
+  return head;
+}
